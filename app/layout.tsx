f97caddc976c9b44600ad0766a820f36bf3f63cf
Muasa-harman <harman.muasa@gmail.com>
2024-08-27 22:57:20 +0300

    co-op bank clone

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter,IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+import { Weight } from "lucide-react";
+
+const inter = Inter({ subsets: ["latin"],variable:'--font-inter' });
+const ibmPlexSerif = IBM_Plex_Serif({subsets: ['latin'], weight: ['400','700']})
+
+export const metadata: Metadata = {
+  title: "co-opbank devportal",
+  description: "Created a portfolio clone",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
+    </html>
+  );
+}
